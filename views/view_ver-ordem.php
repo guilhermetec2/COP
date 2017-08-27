e81550c9b8ff5b6b60fc99be40d33276886562bc
guilhermetec2 <guilhermetec2@gmail.com>
2017-08-26 22:16:31 -0300

    Ajuste da pagina view_ver-ordem

--- views/view_ver-ordem.php
+++ views/view_ver-ordem.php
@@ -25,7 +25,7 @@
             
             <h2 class="w3-margin-bottom w3-margin-left"><i class="fa fa-file w3-xxlarge"></i> Ver Ordem</h2>     
            
-            <section class="w3-card-4 w3-white folha-ordem">
+            <section class="w3-card-4 w3-white folha-ordem w3-margin-bottom">
                 <h3 class="w3-center">Ordem numero: 82666</h3>
                 <table class="w3-table">
                             <tr>
@@ -44,25 +44,29 @@
                                 <td>Prazo:</td>
                                 <td>02/07/2017</td>
                             </tr>
-                            <tr>
-                                <td>Parecer:</td>
-                                <td>dasfkdfjkkjfjdsfjdslfkjlfkjdsfkldsfklfdsfglkdflkdfglçkgflçdçflkfdçgl
-                                    kdfçgkdfglçdflçgfdkgf
-                                
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Orçamento:</td>
-                                <td>dasfkdfjkkjfjdsfjdslfkjlfkjdsfkldsfklfdsfglkdflkdfglçkgflçdçflkfdçgl
-                                    kdfçgkdfglçdflçgfdkgf
-                                
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Atulizado por:</td>
-                                <td>Guilherme Rodrigues - 18/04/2017 - 12:50</td>
-                            </tr>
-                        </table>
+                </table>
+                <div class="w3-panel">
+                    <p>Defeitos Reletados:</p>
+                    <br>
+                    <p>
+                        <textarea disabled>
+                            lkjlkfjdslkjfklçdsjflkdsjfldksjflkdsjfdslfjdfljkdsfkljdsflkjfdsklfjdfjsklçfjsdlfjkdslfkjdsflkdjslsjlsfjsldfkjdsklçfdsjflkdsjfdsljkfdsldskjfdslkfjdsflk
+                        </textarea>
+                    </p>
+                </div>
+
+                <div class="w3-panel">
+                    <p>Orçamento:</p>
+                    <br>
+                    <p>
+                        <textarea disabled>
+                            lkjlkfjdslkjfklçdsjflkdsjfldksjflkdsjfdslfjdfljkdsfkljdsflkjfdsklfjdfjsklçfjsdlfjkdslfkjdsflkdjslsjlsfjsldfkjdsklçfdsjflkdsjfdsljkfdsldskjfdslkfjdsflk
+                        </textarea>
+                    </p>
+                    
+                    <p>Atualizado por Pablo as 10:45 do dia 25/08/2017 </p>
+                </div>
+                
                         <p>
                             <a href="#" class="w3-button w3-blue w3-right">Editar</a>
                         </p>
